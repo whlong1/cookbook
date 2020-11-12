8503d3c74215fc1f237cf5aba1cbe178b5466965
whlong1 <whunterlong@gmail.com>
2020-11-12 15:56:46 -0500

    integrating auth part 1

--- server.js
+++ server.js
@@ -2,16 +2,22 @@ const express = require('express');
 const AppRouter = require('./routes/AppRouter');
 const db = require('./db/index')
 const logger = require('morgan')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const helmet = require('helmet')
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
 //Middleware
 app.use(logger('dev'))
-// app.use(cors())
-const bodyParser = require('body-parser')
+app.use(helmet())
+app.use(cors())
 app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+
 //Middleware
+
 app.get('/', (request, response) => response.send('Root test'))
 
 app.use('/home', AppRouter);
