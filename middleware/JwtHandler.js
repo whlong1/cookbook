e9a6ee10bf7a7f65636df5dc0a3b7e7e81287de6
whlong1 <whunterlong@gmail.com>
2020-11-12 16:09:57 -0500

    integrating auth part 3

--- middleware/JwtHandler.js
+++ middleware/JwtHandler.js
@@ -3,11 +3,26 @@ require('dotenv').config()
 
 const secretKey = process.env.SECRET_KEY
 
-const getToken = (request, response, next) => {}
+const getToken = (request, response, next) => {
+    const token = request.headers ['authorization'].split(' ')[1]
+    response.locals.token = token
+    next()
+}
 
-const verifyToken = (request, response, next) => {}
+const verifyToken = (request, response, next) => {
+    let token = response.locals.token
+    jwt.verify(token, secretKey, (err, t) => {
+        if (err) {
+            return response.status(401).json({msg: 'Invalid Token'})
+        }
+        return next()
+    })
+}
 
-const createToken = (request, response) => {}
+const createToken = (request, response) => {
+    const token = jwt.sign(response.locals.payload, secretKey)
+    response.send({user: response.locals.payload, token})
+}
 
 
 
