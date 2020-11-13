c9e9d724c6e204b516fd322f1be2126ac70804f3
whlong1 <whunterlong@gmail.com>
2020-11-12 21:36:43 -0500

    react setup/cleaned up folder structure

--- routes/ReviewRouter.js
+++ routes/ReviewRouter.js
@@ -5,10 +5,7 @@ const ReviewController = require('../controllers/ReviewController')
 Router.post('/add', ReviewController.AddReview)
 
 Router.get('/search/:id', ReviewController.GetReview)
-
 //GET -> localhost:3000/home/reviews/search/5fad7020ec3f93ff91750439
-
-
 
 Router.delete('/delete/:id', ReviewController.DeleteReview)
 
