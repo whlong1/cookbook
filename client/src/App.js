c55cf254651016305cd4bf54faf60320030bf9ee
whlong1 <whunterlong@gmail.com>
2020-11-13 01:25:28 -0500

    front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,17 @@
+import React from 'react'
+
+import {Route, Switch} from 'react-router-dom'
+
 function App() {
   return (
-    <div className="App"></div>
+    <div className="App">
+      <Header/>
+      <main>
+        <Switch>
+          <Route exact path="/" component={Home}/>
+        </Switch>
+      </main>
+    </div>
   );
 }
 
---
+++ client/src/components/Router.js
@@ -0,0 +1,4 @@
+import { Switch, Route } from 'react-router-dom'
+import Home from '../pages/Home'
+
+
