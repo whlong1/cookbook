cb30afeb168a77a4bfe8e081becbd10080003dee
whlong1 <whunterlong@gmail.com>
2020-11-13 12:38:12 -0500

    initial homepage css

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,21 @@
 import React from 'react'
+import '../styles/Home.css'
 
 export default () => {
   return (
-    <div className="homepage_layout">CookBook</div>
+    <div className="homepage">
+        <div className="homepage-left">
+            <div className="welcome-msg"><h1>Welcome to CookBook</h1></div>
+            <div className="yellow-block"></div>
+        </div>
+        <div className="homepage-right">
+            <div className="link-container">
+                <div></div>
+            </div>
+        </div>
+    </div>
   )
 }
+
+
+/* <h1 className="title"></h1> */
