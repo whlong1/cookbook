7890a41f26f3577103c03bf11a021336992043f6
whlong1 <whunterlong@gmail.com>
2020-11-13 19:09:23 -0500

    Revert "added all plate icons"
    This reverts commit e962db199017704e331b8896e881f9df0d50eff8.

--- client/src/components/FoodList.js
+++ client/src/components/FoodList.js
@@ -2,11 +2,6 @@ import React from 'react'
 import '../styles/FoodList.css'
 import BackgroundImg from '../assets/background.jpg'
 import Plate1 from '../assets/avocadotoast.png'
-import Plate2 from '../assets/pancakes.png'
-import Plate3 from '../assets/pasta.png'
-import Plate4 from '../assets/curry.png'
-import Plate5 from '../assets/salad.png'
-import Plate6 from '../assets/cake.png'
 
 
 export default () => {
@@ -14,13 +9,12 @@ export default () => {
     <div>
         <div>
             <img src={Plate1} alt="avocado toast"/>
-            <img src={Plate2} alt="pancakes"/>
-            <img src={Plate3} alt="pasta"/>
-            <img src={Plate4} alt="curry"/>
-            <img src={Plate5} alt="salad"/>
-            <img src={Plate6} alt="cake"/>
         </div>
     </div>
   )
 }
 
+
+/* <div>
+<img src={Plate1} alt="avocado toast"/>
+</div> */
