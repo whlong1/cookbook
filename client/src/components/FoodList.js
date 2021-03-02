995bd7f49241fd7046249315b2b87e467737d3ab
whlong1 <whunterlong@gmail.com>
2021-03-02 13:31:51 -0500

    {FoodList.js}  conditionally render button overlays

--- client/src/components/FoodList.js
+++ client/src/components/FoodList.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import '../styles/FoodList.css'
 
@@ -11,16 +11,55 @@ import Plate6 from '../assets/cake.png'
 
 
 export default () => {
+
+  const [d1, setD1] = useState('invisible')
+  const [d2, setD2] = useState('invisible')
+
+  const showButton = e => {
+    e.preventDefault();
+    console.log(e.target.title)
+
+    switch(e.target.title){
+      case "Health Food":
+        setD1('visible')
+        break
+      case "Breakfast":
+        setD2('visible')
+        break
+      default:
+        //
+    }
+  };
+
+  const hideButton = e => {
+    e.preventDefault();
+    setD1('invisible')
+    setD2('invisible')
+  };
+
   return (
     <div className="listcontainer">
       <div className="titleContainer">
-      <h1 className="samplePlateTitle">Cuisine</h1>
+        <h1 className="samplePlateTitle">Cuisine</h1>
 
       </div>
       <div className="plateContainer">
         <div className="columnA">
-          <Link to="/home/recipes/sort/HEALTHY"><img className="plate" src={Plate1} alt="avocado toast" /></Link>
-          <Link to="/home/recipes/sort/BREAKFAST"><img className="plate" src={Plate2} alt="pancakes" /></Link>
+
+
+
+          <Link className="plate" to="/home/recipes/sort/HEALTHY" onMouseEnter={e => showButton(e)} onMouseLeave={e => hideButton(e)}>
+            <button className={d1}>Health Food</button>
+            <img title="Health Food" src={Plate1} alt="avocado toast" />
+          </Link>
+
+
+          <Link className="plate" to="/home/recipes/sort/BREAKFAST" onMouseEnter={e => showButton(e)} onMouseLeave={e => hideButton(e)}>
+            <button className={d2}>Breakfast</button>
+            <img title="Breakfast" src={Plate2} alt="pancakes" />
+          </Link>
+
+
           <Link><img className="plate" src={Plate3} alt="pasta" /></Link>
         </div>
 
