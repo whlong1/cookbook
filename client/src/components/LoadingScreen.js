44274abb5765ac19cc456ee4e25ecf49b938a340
whlong1 <whunterlong@gmail.com>
2021-03-09 17:12:35 -0500

    {LoadingScreen.js} create loading screen component

---
+++ client/src/components/LoadingScreen.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+// import '../styles/CardDesign.css'
+
+
+export default class LoadingScreen extends Component {
+
+
+  render() {
+
+    return (
+      <div>
+          <img></img>
+          <h1>Loading</h1>
+      </div>
+    )
+
+  }
+
+}
+
+
--- client/src/pages/AllCuisine.js
+++ client/src/pages/AllCuisine.js
@@ -1,43 +1,42 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 
 
 import '../styles/ListPages.css'
 // import Panel from '../assets/panels4.jpg'
 
 import CuisineCard from '../components/CuisineCard'
+import LoadingScreen from '../components/LoadingScreen'
 
 export default class AllCuisine extends Component {
 
 
 
 
-render() {
-  console.log(this.props.cuisine)
-  const cuisine = this.props.cuisine
-  return (
-    <div className="pageLayout">
-      {this.props.cuisine ? 
-      <div className="pageSpread">
-      <button className="backButtonL" onClick={() => this.props.history.goBack()}>Back</button>
-        <div className="cardContainer">
-          {cuisine.map((cuisine) => (
-            <CuisineCard
-              key={cuisine._id}
-              onClick={() => this.props.history.push(`/home/recipes/sort/${cuisine.name}`)}
-              name={cuisine.name}
-              image={cuisine.image}
-              recipes={cuisine.recipes}
-            />
-          ))}
-        </div>        
+  render() {
+    console.log(this.props.cuisine)
+    const cuisine = this.props.cuisine
+    return (
+      <div className="pageLayout">
+        {this.props.cuisine ?
+          <div className="pageSpread">
+            <button className="backButtonL" onClick={() => this.props.history.goBack()}>Back</button>
+            <div className="cardContainer">
+              {cuisine.map((cuisine) => (
+                <CuisineCard
+                  key={cuisine._id}
+                  onClick={() => this.props.history.push(`/home/recipes/sort/${cuisine.name}`)}
+                  name={cuisine.name}
+                  image={cuisine.image}
+                  recipes={cuisine.recipes}
+                />
+              ))}
+            </div>
+          </div>
+          :
+          <div><LoadingScreen /></div>
+        }
       </div>
-      :
-      <div>Loading</div>
-      
-    }
-
-    </div>
-  )
-}
+    )
+  }
 }
 
