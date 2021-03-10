69dd66b7c087bfdc0d70c54f8ad8ad26cb2cd9bb
whlong1 <whunterlong@gmail.com>
2021-03-10 11:41:10 -0500

    {LoadingScreen.js} clean up/simplify jsx

--- client/src/components/LoadingScreen.js
+++ client/src/components/LoadingScreen.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react'
 import '../styles/LoadingScreen.css'
-import CutleryIcon from '../assets/Cutlery.png'
 import HatIcon from '../assets/Hat.png'
-
-
 
 
 
@@ -15,36 +12,13 @@ export default class LoadingScreen extends Component {
 
         return (
             <div className="loadingContainer">
-                {this.props.cuisine ?
-                    <div className="loading">
-                        {/* <img className="iconTwo" src={CutleryIcon}></img> */}
-                        <img className="iconOne" src={HatIcon}></img>
-                        <h3 className="loadingText">Preparing Your Menu</h3>
-                    </div>
-
-                    :
-                    <div>
-                        {this.props.recipes ?
-                            <div className="loading">
-                                {/* <img className="iconTwo" src={CutleryIcon}></img> */}
-                                <img className="iconOne" src={HatIcon}></img>
-                                <h3 className="loadingText">Finding Recipes</h3>
-                            </div>
-                            :
-                            <div className="loading">
-                            {/* <img className="iconTwo" src={CutleryIcon}></img> */}
-                            <img className="iconOne" src={HatIcon}></img>
-                            <h3 className="loadingText">Preparing Your Menu</h3>
-                        </div>
-                        }
-                    </div>
-
-                }
+                <div className="loading">
+                    <img className="iconOne" src={HatIcon}></img>
+                    <h3 className="loadingText">Preparing Your Order</h3>
+                </div>
             </div>
         )
-
     }
-
 }
 
 
