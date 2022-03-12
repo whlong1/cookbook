84ddffd5eb6a09c7757283774711ff92be27be07
whlong1 <whunterlong@gmail.com>
2022-03-12 03:33:28 -0500

    add condition for singular/plural result text

--- client/src/components/CuisineCard.js
+++ client/src/components/CuisineCard.js
@@ -9,11 +9,15 @@ export default class CuisineCard extends Component {
       backgroundSize: "cover"
     }
 
+    const result = this.props.recipes.length > 1 || !this.props.recipes.length
+      ? 'results'
+      : 'result'
+
     return (
       <div style={imageStyle} className="card" onClick={this.props.onClick}>
         <div className="card-text">
           <h3>{this.props.name}</h3>
-          <p>{this.props.recipes.length} Results</p>
+          <p>{this.props.recipes.length} {result}</p>
         </div>
       </div>
     )
